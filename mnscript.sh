ff136ca5dc2d10391cd2ccf5ffd7da10b3f7e6e5
bidxcore <49323072+bidxcore@users.noreply.github.com>
2019-06-09 02:14:39 +0300

    Add files via upload

--- mnscript.sh
+++ mnscript.sh
@@ -1,65 +1,29 @@
-    
 #!/bin/bash
-#
-# masternode from scratch (baz 20032019)
-# i remember a time when youd get knocked out asking for binaries (!)
-
-# check if we have swap
-memory="$(free | grep Swap | tr -s ' ' | cut -d ' ' -f 4)"
-if [ -n "$memory" ] && [ "$memory" -eq "$memory" ] 2>/dev/null; then
-  if [ "$memory" -eq "0" ]; then
-      # make a quick 1gb swap as we only need it for compilation
-      dd if=/dev/zero of=/var/swap.img bs=1024k count=1000
-          mkswap /var/swap.img
-          swapon /var/swap.img
-          echo enabled temporary swapfile..
-    else
-          # a suitable swap file exists
-      echo swapfile not required..
-  fi
-else
-  # if we dont understand this, update linux
-  An update to latest Linux is required.
-  exit
-fi
-
-# install the dependencies
-apt update
-apt install -y build-essential autoconf automake libssl-dev libdb5.3-dev libdb5.3++-dev libboost-all-dev pkg-config libtool libevent-dev git screen
-
-# build fusionx
-git clone https://github.com/bidxcore/bidx
-cd FusionX
-./autogen.sh
-./configure --with-incompatible-bdb --disable-tests
-make install
-cd
+wget https://github.com/bidxcore/releases/raw/master/linux/bidx-linux64.tgz
+tar xvf bidx-linux64.tgz
+cp -f bidx-linux64/* /usr/local/bin
 
 # find our public ip
 primaryip="$(ip route get 1 | awk '{print $NF;exit}')"
-echo "This is where you paste the masternode privkey (output from 'masternode genkey') and press enter"
+echo "paste the masternode privkey (output from 'masternode genkey') and press enter"
 read -e masternodeprivkey
 
-# write our masternode's .fusion/fusion.conf
-mkdir .fusion
-echo listen=1 > .fusion/fusion.conf
-echo server=1 >> .fusion/fusion.conf
-echo daemon=1 >> .fusion/fusion.conf
-echo staking=0 >> .fusion/fusion.conf
-echo rpcuser=testuser >> .fusion/fusion.conf
-echo rpcpassword=testpassword >> .fusion/fusion.conf
-echo rpcallowip=127.0.0.1 >> .fusion/fusion.conf
-echo rpcbind=127.0.0.1 >> .fusion/fusion.conf
-echo maxconnections=24 >> .fusion/fusion.conf
-echo masternode=1 >> .fusion/fusion.conf
-echo masternodeprivkey=$masternodeprivkey >> .fusion/fusion.conf
-echo bind=$primaryip >> .fusion/fusion.conf
-echo externalip=$primaryip >> .fusion/fusion.conf
-echo masternodeaddr=$primaryip:24999 >> .fusion/fusion.conf
+# write our masternode's .BIDX/bidx.conf
+rm -f .BIDX/bidx.conf
+mkdir .BIDX
+echo listen=1 > .BIDX/bidx.conf
+echo server=1 >> .BIDX/bidx.conf
+echo daemon=1 >> .BIDX/bidx.conf
+echo staking=0 >> .BIDX/bidx.conf
+echo rpcuser=testuser >> .BIDX/bidx.conf
+echo rpcpassword=testpassword >> .BIDX/bidx.conf
+echo rpcallowip=127.0.0.1 >> .BIDX/bidx.conf
+echo rpcbind=127.0.0.1 >> .BIDX/bidx.conf
+echo maxconnections=24 >> .BIDX/bidx.conf
+echo masternode=1 >> .BIDX/bidx.conf
+echo masternodeprivkey=$masternodeprivkey >> .BIDX/bidx.conf
+echo bind=$primaryip >> .BIDX/bidx.conf
+echo externalip=$primaryip >> .BIDX/bidx.conf
+echo masternodeaddr=$primaryip:40000 >> .BIDX/bidx.conf
 
-# sleep because sleeping is good.. sometimes
-sleep 1
-fusiond
-
-# finished
-echo finished
+bidxd
