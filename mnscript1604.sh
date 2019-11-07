361264899eab07fedf9e6311a3ccf6560fecff25
bidxcore <49323072+bidxcore@users.noreply.github.com>
2019-11-06 18:59:07 -0600

    Update mnscript1604.sh

--- mnscript1604.sh
+++ mnscript1604.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# This is script to run masternode on Ubuntu 16.04
+# to run this script highlight and copy: 
+#   wget https://raw.githubusercontent.com/bidxcore/bidx-MNScript/master/mnscript1604.sh && bash mnscript1604.sh
+
 rm -rf bidx-ubuntu1604-b63a74d3d.tgz
 wget https://github.com/bidxcore/bidx/releases/download/b63a74d3d/bidx-ubuntu1604-b63a74d3d.tgz
 rm -rf bidx-ubuntu1604-b63a74d3d
